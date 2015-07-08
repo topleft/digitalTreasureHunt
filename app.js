ba0f52beada4b04b1a1d75544ff959dcd78826fc
porkpie <porkpie@hat.com>
2015-07-08 09:01:11 -0600

    Added a hint image on mouseenter, treasure on click.

--- app.js
+++ app.js
@@ -2,11 +2,29 @@
 
 $(document).ready( function () {
 	$('.treasure-box').on('mouseenter', function () {
-		$(this).find('div').show();
+		$(this).find('img').show();
+
+		// $(this).find('.hint').on('click', function () {
+		// 	$(this).closest('.treasure-box').find('.hidden-treasure').show();
+		// 	$(this).hide();
+		// });
 
 	});
+
+	$('.hint').on('click', function () {
+		$(this).next().show();
+		$(this).hide();
+
+	});
+
+
+
 	$('.treasure-box').on('mouseleave', function () {
-		$(this).find('div').hide();
+		$(this).find('img').hide();
+		$(this).find('.hidden-treasure').hide();
 
 	});
 });
+
+// Add treasure box or heart that pops up on hover and then change
+// the picture to show up on click
